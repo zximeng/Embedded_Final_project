edcf7e10d30711191b66709c62fe2fc63a4f85c9
ximeng zhang <46571875+zximeng@users.noreply.github.com>
2020-04-23 15:34:25 -0400

    Continuous reading of XYZ on both IMU

--- i2c_imu.py
+++ i2c_imu.py
@@ -1,4 +1,3 @@
-
 import smbus
 import time
 bus = smbus.SMBus(1)
@@ -19,72 +18,75 @@ STAT = 0x22
 INT1 = 0x0D
 CTRL1 = 0x10
 ACCX = 0x28
+ACCXH = 0x29
+ACCY = 0x2A
+ACCYH = 0x2B
+ACCZ = 0x2C
+ACCZH = 0x2D
 STAT_REG = 0x1E
 MASTER_MAIN = 0x39
 SENSOR_1 = 0x02
 
-# slave SD0 connected to VDD
 
-# First initialize the master device, then use the sensor hub to
-# initialize the slave device. Read from the slave device, save to FIFO, read 
-# from master device and read from FIFO. 
+def one_shot_write(register, value):
+	bus.write_byte_data(0x6a,FUNC,0x40)
+	bus.write_byte_data(0x6a,SLAV,slave_addr_write)
+	bus.write_byte_data(0x6a,SLAVS,register)
+	bus.write_byte_data(0x6a,SLAV_CONF,0x00)
+	bus.write_byte_data(0x6a,DATAW,value)
+	bus.write_byte_data(0x6a,MAST,0x4C)
+	while (not bus.read_byte_data(0x6a,STAT)>>7):
+		continue
+	bus.write_byte_data(0x6a,MAST,0x08)
+	time.sleep(0.0003)
+	bus.write_byte_data(0x6a,FUNC,0x00)
+
+def one_shot_read(register):
+	bus.write_byte_data(0x6a,FUNC,0x40)
+	bus.write_byte_data(0x6a,SLAV,slave_addr_read)
+	bus.write_byte_data(0x6a,SLAVS,register)
+	bus.write_byte_data(0x6a,SLAV_CONF,0x01)
+	bus.write_byte_data(0x6a,MAST,0x4C)
+	bus.write_byte_data(0x6a,FUNC,0x00)
+	accx = bus.read_word_data(0x6a,0x29) # clear data ready on OUTX_H_A
+	# poll data ready on status_reg
+	while (not bus.read_byte_data(0x6a,STAT_REG) & 0x01):
+		continue
+	while (not bus.read_byte_data(0x6a,MASTER_MAIN) & 0x01):
+		continue
+	bus.write_byte_data(0x6a,FUNC,0x40)
+	bus.write_byte_data(0x6a,MAST,0x08)
+	time.sleep(0.0003)
+	accx = bus.read_byte_data(0x6a,SENSOR_1)
+
+	bus.write_byte_data(0x6a,FUNC,0x00)
+	return accx
+	
+
 bus.write_byte_data(0x6a,INT1,0x01)
 bus.write_byte_data(0x6a,CTRL1,0x40)
 
 # one shot write to INT1 on slave IMU 
-bus.write_byte_data(0x6a,FUNC,0x40)
-bus.write_byte_data(0x6a,SLAV,slave_addr_write)
-bus.write_byte_data(0x6a,SLAVS,INT1)
-bus.write_byte_data(0x6a,SLAV_CONF,0x00)
-bus.write_byte_data(0x6a,DATAW,0x01)
-bus.write_byte_data(0x6a,MAST,0x4C)
-while (not bus.read_byte_data(0x6a,STAT)>>7):
-	continue
-bus.write_byte_data(0x6a,MAST,0x08)
-time.sleep(0.0003)
-bus.write_byte_data(0x6a,FUNC,0x00)
+one_shot_write(INT1,0x01)
 
 # One shot write to CTRL
-bus.write_byte_data(0x6a,FUNC,0x40)
-bus.write_byte_data(0x6a,SLAV,slave_addr_write)
-bus.write_byte_data(0x6a,SLAVS,CTRL1)
-bus.write_byte_data(0x6a,SLAV_CONF,0x00)
-bus.write_byte_data(0x6a,DATAW,0x60)
-bus.write_byte_data(0x6a,MAST,0x4C)
-while (not bus.read_byte_data(0x6a,STAT)>>7):
-	continue
-bus.write_byte_data(0x6a,MAST,0x08)
-time.sleep(0.0003)
-bus.write_byte_data(0x6a,FUNC,0x00)
+one_shot_write(CTRL1,0x60)
 
-print('the data from the second IMU:')
-# One shot read from 2nd IMU 
-bus.write_byte_data(0x6a,FUNC,0x40)
-bus.write_byte_data(0x6a,SLAV,slave_addr_read)
-bus.write_byte_data(0x6a,SLAVS,ACCX)
-bus.write_byte_data(0x6a,SLAV_CONF,0x01)
-bus.write_byte_data(0x6a,MAST,0x4C)
-bus.write_byte_data(0x6a,FUNC,0x00)
-accx = bus.read_word_data(0x6a,0x29) # clear data ready on OUTX_H_A
-# poll data ready on status_reg
-while (not bus.read_byte_data(0x6a,STAT_REG) & 0x01):
-	continue
-while (not bus.read_byte_data(0x6a,MASTER_MAIN) & 0x01):
-	continue
-bus.write_byte_data(0x6a,FUNC,0x40)
-bus.write_byte_data(0x6a,MAST,0x08)
-time.sleep(0.0003)
-accx = bus.read_word_data(0x6a,SENSOR_1)
-print(accx)
-bus.write_byte_data(0x6a,FUNC,0x00)
 
-def read_data():
-	accx = bus.read_word_data(0x6a,ACCX)
+
+def read_data(register):
+	accx = bus.read_word_data(0x6a,register)
 	return accx
-print('the data from the first IMU:')
 
 
-for i in range(0,10):
-	print(read_data())
-	time.sleep(1)
+
+for i in range(0,1):
 	
+
+	X = one_shot_read(ACCX) | one_shot_read(ACCXH)<<8
+	X = one_shot_read(ACCY) | one_shot_read(ACCYH)<<8
+	X = one_shot_read(ACCZ) | one_shot_read(ACCZH)<<8
+	read_data(ACCX)
+	read_data(ACCY)
+	read_data(ACCZ)
+
