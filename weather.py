79eda2bc019abe7dc3136ef9bc04edd922ab9343
ximeng zhang <46571875+zximeng@users.noreply.github.com>
2020-04-27 12:15:41 -0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,10 @@
+import requests
+
+api = 'http://api.openweathermap.org/data/2.5/weather?zip=14850,us&appid=818eb27f395ca0f860e5ce34fedade0f'
+data = requests.get(api).json()
+weather = data['weather'][0]['main']
+print(weather)
+temp = data['main']['temp']-273.15
+print(str(temp)[:5]+ 'C')
+hum = data['main']['humidity']
+print(hum)
