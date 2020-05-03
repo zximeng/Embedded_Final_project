eb7ee862d2f5e4742b674f560148c15ccfda5c46
ximeng zhang <46571875+zximeng@users.noreply.github.com>
2020-05-02 21:08:42 -0400

    Update i2c_imu.py

--- i2c_imu.py
+++ i2c_imu.py
@@ -8,6 +8,7 @@ slave_addr_read = 0b11010111
 # write |0
 # read |1
 slave_addr_write=  0b11010110
+
 FUNC = 0x01
 SLAV = 0x15
 SLAVS = 0x16
@@ -26,67 +27,68 @@ ACCZH = 0x2D
 STAT_REG = 0x1E
 MASTER_MAIN = 0x39
 SENSOR_1 = 0x02
+FIFO_CTRL3 = 0x09
+FIFO_CTRL4 = 0x0A
+CTRL6_C = 0x15
+CTRL9_XL = 0x18
+CRTL1_XL = 0x10
 
 
-def one_shot_write(register, value):
-	bus.write_byte_data(0x6a,FUNC,0x40)
-	bus.write_byte_data(0x6a,SLAV,slave_addr_write)
-	bus.write_byte_data(0x6a,SLAVS,register)
-	bus.write_byte_data(0x6a,SLAV_CONF,0x00)
-	bus.write_byte_data(0x6a,DATAW,value)
-	bus.write_byte_data(0x6a,MAST,0x4C)
-	while (not bus.read_byte_data(0x6a,STAT)>>7):
-		continue
-	bus.write_byte_data(0x6a,MAST,0x08)
-	time.sleep(0.0003)
-	bus.write_byte_data(0x6a,FUNC,0x00)
+bus.write_byte_data(0x6a,FIFO_CTRL3,0x04)
+# enable acc reading at 104 Hz
 
-def one_shot_read(register):
-	bus.write_byte_data(0x6a,FUNC,0x40)
-	bus.write_byte_data(0x6a,SLAV,slave_addr_read)
-	bus.write_byte_data(0x6a,SLAVS,register)
-	bus.write_byte_data(0x6a,SLAV_CONF,0x01)
-	bus.write_byte_data(0x6a,MAST,0x4C)
-	bus.write_byte_data(0x6a,FUNC,0x00)
-	accx = bus.read_word_data(0x6a,0x29) # clear data ready on OUTX_H_A
-	# poll data ready on status_reg
-	while (not bus.read_byte_data(0x6a,STAT_REG) & 0x01):
-		continue
-	while (not bus.read_byte_data(0x6a,MASTER_MAIN) & 0x01):
-		continue
-	bus.write_byte_data(0x6a,FUNC,0x40)
-	bus.write_byte_data(0x6a,MAST,0x08)
-	time.sleep(0.0003)
-	accx = bus.read_byte_data(0x6a,SENSOR_1)
+bus.write_byte_data(0x6a,FIFO_CTRL4,0x06)
+# set fifo in continuous mode 
 
-	bus.write_byte_data(0x6a,FUNC,0x00)
-	return accx
+bus.write_byte_data(0x6a,0x5E,0x01)
+# enable sensor hub data-ready on INT1
+
+bus.write_byte_data(0x6a,CTRL6_C,0x80)
+# INT2 input mode
+
+#bus.write_byte_data(0x6a,CTRL9_XL,0xE8)
+#extend DEN 
+
+
+
+
+
+
+# read from sensor hub
+
+
+bus.write_byte_data(0x6a,FUNC,0x40)
+bus.write_byte_data(0x6a,SLAV,slave_addr_read)
+bus.write_byte_data(0x6a,SLAVS,ACCX)
+bus.write_byte_data(0x6a,SLAV_CONF,0x0E)
+bus.write_byte_data(0x6a,MAST,0x4C)
+bus.write_byte_data(0x6a,FUNC,0x00)
+accx = bus.read_word_data(0x6a,0x29) # clear data ready on OUTX_H_A
+# poll data ready on status_reg
+while (not bus.read_byte_data(0x6a,STAT_REG) & 0x01):
+	continue
+while (not bus.read_byte_data(0x6a,MASTER_MAIN) & 0x01):
+	continue
+bus.write_byte_data(0x6a,FUNC,0x40)
+bus.write_byte_data(0x6a,MAST,0x08)
+
 	
 
-bus.write_byte_data(0x6a,INT1,0x01)
-bus.write_byte_data(0x6a,CTRL1,0x40)
-
-# one shot write to INT1 on slave IMU 
-one_shot_write(INT1,0x01)
-
-# One shot write to CTRL
-one_shot_write(CTRL1,0x60)
 
 
+#get level and do 10 read from fifo
 
-def read_data(register):
-	accx = bus.read_word_data(0x6a,register)
-	return accx
-
-
-
-for i in range(0,1):
+level = bus.read_byte_data(0x6a,0x3A)
+print(level)
+count = 3
+while (level or count) :
 	
-
-	X = one_shot_read(ACCX) | one_shot_read(ACCXH)<<8
-	X = one_shot_read(ACCY) | one_shot_read(ACCYH)<<8
-	X = one_shot_read(ACCZ) | one_shot_read(ACCZH)<<8
-	read_data(ACCX)
-	read_data(ACCY)
-	read_data(ACCZ)
+	IMU2 = bus.read_i2c_block_data(0x6a,0x78,7)
+	print(IMU2)
+	if IMU2[0] != 17 and  IMU2[0] != 18 and  IMU2[0] != 20 and  IMU2[0] != 23 :
+		print(IMU2)
+	else:
+		print('no luck')
+	count -= 1
+	
 
