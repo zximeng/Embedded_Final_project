b9d9954401f137da7111d6fe09d4011a226457b9
ximeng zhang <46571875+zximeng@users.noreply.github.com>
2020-05-13 00:37:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import os
 from pygame.locals import *   # for event MOUSE variables
 from yahoo_fin.stock_info import *
 import subprocess
+from pygame_gui.ui_manager import UIManager
+from pygame_gui.elements.ui_text_box import UITextBox
+from pygame_gui.core import IncrementalThreadedResourceLoader
 proc = 0
 
 def current_weather():
@@ -108,6 +111,7 @@ def weather_menu():
 
 
 pygame.init()
+pygame.display.set_caption("Simon's Home")
 pygame.mouse.set_visible(False)
 WHITE = 255, 255, 255
 BLACK = 0,0,0
@@ -115,19 +119,67 @@ RED=255,0,0
 
 green = (0, 255, 0) 
 blue = (0, 0, 128) 
-screen = pygame.display.set_mode((320, 240))
-my_font= pygame.font.Font(None, 20)
+screen_size = (320,240)
+screen = pygame.display.set_mode(screen_size)
+my_font= pygame.font.Font("COMIC.TTF", 13)
 my_font1 = pygame.font.Font(None,30)
 my_buttons= { 'quit':(80,180), 'system':(240,180)}
 start = time.time()
 weather_position = [(30,30),(85,25),(120,25)]
+background_surface = pygame.Surface(screen_size)
+background_surface.fill(pygame.Color("#000000"))
+
+loader = IncrementalThreadedResourceLoader()
+clock = pygame.time.Clock()
+ui_manager = UIManager(screen_size, 'data/themes/theme_1.json', resource_loader=loader)
+ui_manager.add_font_paths("Montserrat",
+						  "data/fonts/Montserrat-Regular.ttf",
+						  "data/fonts/Montserrat-Bold.ttf",
+						  "data/fonts/Montserrat-Italic.ttf",
+						  "data/fonts/Montserrat-BoldItalic.ttf")
+
+ui_manager.preload_fonts([{'name': 'Montserrat', 'html_size': 4.5, 'style': 'bold'},
+						  {'name': 'Montserrat', 'html_size': 4.5, 'style': 'regular'},
+						  {'name': 'Montserrat', 'html_size': 2, 'style': 'regular'},
+						  {'name': 'Montserrat', 'html_size': 2, 'style': 'italic'},
+						  {'name': 'Montserrat', 'html_size': 6, 'style': 'bold'},
+						  {'name': 'Montserrat', 'html_size': 6, 'style': 'regular'},
+						  {'name': 'Montserrat', 'html_size': 6, 'style': 'bold_italic'},
+						  {'name': 'Montserrat', 'html_size': 4, 'style': 'bold'},
+						  {'name': 'Montserrat', 'html_size': 4, 'style': 'regular'},
+						  {'name': 'Montserrat', 'html_size': 4, 'style': 'italic'},
+						  {'name': 'fira_code', 'html_size': 2, 'style': 'regular'},
+						  {'name': 'fira_code', 'html_size': 2, 'style': 'bold'},
+						  {'name': 'fira_code', 'html_size': 2, 'style': 'bold_italic'}
+						  ])
+loader.start()
+finished_loading = False
+while not finished_loading:
+	finished_loading, progress = loader.update()
 
 flag = True
+counter = 0
+inits = [0,10]
+inits1 = [200,10]
 while flag:  
-	screen.fill(BLACK) 
+	screen.fill(BLACK)
+	screen.blit(background_surface, (0, 0))
+	ui_manager.draw_ui(screen)
+	#======================================================
+	#
+	#======================================================
 	# display date and time
-	date = display_date() 
-	text = my_font1.render(str(date[4]), True, RED)
+	
+	
+	date = display_date()
+	# display a moon if its late in the night 
+	if int(str(date[3][:2])) >= 21:
+		ball1 = pygame.image.load("moon.png")
+		ballrect1 = ball1.get_rect()
+		ballrect1 = ballrect1.move(inits1)
+	 
+	screen.blit(ball1, ballrect1)
+	text = my_font1.render(str(date[3]), True, RED)
 	textRect = text.get_rect(center=(250,30))  
 	screen.blit(text, textRect)
 	text = my_font1.render(str(date[0]), True, green)
@@ -136,39 +188,91 @@ while flag:
 	text = my_font.render(str(date[1]), True, WHITE)
 	textRect = text.get_rect(center=(200,50))  
 	screen.blit(text, textRect)
-	text = my_font.render(str(date[3]), True, WHITE)
+	text = my_font.render(str(date[2]), True, WHITE)
 	textRect = text.get_rect(center=(225,50))  
 	screen.blit(text, textRect)
+	text = my_font.render(str(date[4]), True, WHITE)
+	textRect = text.get_rect(center=(260,50))  
+	screen.blit(text, textRect)
+
+
+
+	#======================================================
+	# display functions   
+	for my_text, text_pos in my_buttons.items():    
+		text_surface = my_font.render(my_text, True, WHITE)    
+		rect = text_surface.get_rect(center=text_pos)
+		screen.blit(text_surface, rect) 
+	#=======================================================
+	#display weather
+	#update frequency: every 10 minutes
+	if counter == 0 or counter % 6000 == 0:
+		weather = current_weather()
+		print('update weather')
+		print(weather[0])
+	if 'Rain' in weather[0] : 
+		ball = pygame.image.load("Rain.png")
+		ballrect = ball.get_rect()
+		ballrect = ballrect.move(inits)
+		
+	elif 'Sunny' in weather[0]:
+		ball = pygame.image.load("sun.png")
+		ballrect = ball.get_rect()
+		ballrect = ballrect.move(inits)
+	elif 'Wind' in weather[0]:
+		ball = pygame.image.load("Wind.png")
+		ballrect = ball.get_rect()
+		ballrect = ballrect.move(inits)
+	elif 'now' in weather[0]:
+		ball = pygame.image.load("Snow.png")
+		ballrect = ball.get_rect()
+		ballrect = ballrect.move(inits)
+	elif 'louds' in weather[0]:
+		ball = pygame.image.load("Cloud.png")
+		ballrect = ball.get_rect()
+		ballrect = ballrect.move(inits)
+	elif 'unde' in weather[0]:
+		ball = pygame.image.load("Thunder.png")
+		ballrect = ball.get_rect()
+		ballrect = ballrect.move(inits)
+	else:
+		text = my_font1.render(str(weather[0]), True, green)
+		textRect = text.get_rect(center=weather_position[0])  
+		screen.blit(text, textRect)
+	screen.blit(ball, ballrect)
+	
+	for i in range(1,3):
+		text = my_font.render(str(weather[i]), True, WHITE)
+		textRect = text.get_rect(center=weather_position[i])  
+		screen.blit(text, textRect)
+	#==================================================
+	#display room temperature
+	#update frequency: every 10 minutes	
+	if counter == 0 or counter % 6000 == 0:
+		temp_data = room_temp()
+	temp = 'room temp: '+ str(temp_data) + '°C'
+	text = my_font.render(temp, True, WHITE)
+	textRect = text.get_rect(center=(60,60))  
+	screen.blit(text, textRect)
+	#=======================================================
+	#print To-do list 
+	
+	htm_text_block_2 = UITextBox('<font face=fira_code size=2 color=#000000><b>To-Do</b>'
+							 '<br><br>'
+							 '5725 Lab and project' '<br><br>''research',
+							 pygame.Rect((150, 70), (100, 80)),
+							 manager=ui_manager,
+							 object_id="#text_box_2")
+	#======================================================
+	# Detect touch on screen to determine next move
 	for event in pygame.event.get():        
 		if(event.type is MOUSEBUTTONDOWN): 
 			pos = pygame.mouse.get_pos()           
 		elif(event.type is MOUSEBUTTONUP):
-			# Erase the Work space 
-			# display functions   
-			for my_text, text_pos in my_buttons.items():    
-				text_surface = my_font.render(my_text, True, WHITE)    
-				rect = text_surface.get_rect(center=text_pos)
-				screen.blit(text_surface, rect)            
+			# Erase the Work space            
 			pos = pygame.mouse.get_pos() 
 			x,y = pos
-			#display weather
-			weather = current_weather()
-			text = my_font1.render(str(weather[0]), True, green)
-			textRect = text.get_rect(center=weather_position[0])  
-			screen.blit(text, textRect)
-			for i in range(1,3):
-				text = my_font.render(str(weather[i]), True, WHITE)
-				textRect = text.get_rect(center=weather_position[i])  
-				screen.blit(text, textRect)
-			#display room temperature
-			
-			temp_data = room_temp()
-			print(temp_data)
-			temp = 'room temp: '+ str(temp_data) + '°C'
-			text = my_font.render(temp, True, WHITE)
-			textRect = text.get_rect(center=(60,60))  
-			screen.blit(text, textRect)
-			pygame.display.flip()       
+			# go to the next level if pressed button.
 			if y > 170:                
 				if x < 160:    
 					print ('quit button pressed')
@@ -177,5 +281,14 @@ while flag:
 					function_menu()
 			elif y < 40 and x < 110 :
 				weather_menu()
+			elif 70 < y < 150 and  100 < x < 250:
+				 function_menu()
+		ui_manager.process_events(event)
 	if(not time.time() - start < 300):
 		flag = False
+	pygame.display.flip() 
+	#update counter every 0.1 second. Refresh counter every hour.
+	time.sleep(0.1)
+	counter += 1
+	if counter > 36000:
+		counter = 0
