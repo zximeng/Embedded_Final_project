242ac520a1a73f41909631168c79eaf030d96fb8
zximeng <simonzhang8525@gmail.com>
2020-06-04 14:46:53 -0400

    used bit pattern to represent values and changed to symbolic names

--- i2cFIFO.py
+++ i2cFIFO.py
@@ -4,19 +4,22 @@ import time
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 bus = smbus.SMBus(1)
-addr = 0x6a
+
+# I2C address and register addresses
+
+IMU_addr = 0x6a
 # master SD0 connected to GND
 slave_addr_read = 0b11010111
 # 0x6b = 1101011
 # write |0
 # read |1
 slave_addr_write=  0b11010110
-FUNC = 0x01
+FUNC_CFG_ACCESS  = 0x01
 SLAV = 0x15
 SLAVS = 0x16
 SLAV_CONF = 0x17
 DATAW = 0x21
-MAST = 0x14
+MASTER_CONFIG = 0x14
 STAT = 0x22
 INT1 = 0x0D
 CTRL1 = 0x10
@@ -34,51 +37,109 @@ FIFO_CTRL4 = 0x0A
 CTRL6_C = 0x15
 CTRL9_XL = 0x18
 CRTL1_XL = 0x10
+MD1_CFG = 0x5E
+FIFO_STATUS1 = 0x3A
+FIFO_DATA_OUT_TAG = 0x78
 
+#bit patterns
+
+#INT1 
+INT1_DRDY_XL = 0x01
+
+#INT2
+INT2_DRDY_XL = 0x01
+
+#CTRL1_XL
+ODR_XL3 = 0x80
+ODR_XL2 = 0x40
+ODR_XL1 = 0x20
+ODR_XL0 = 0x10
+
+#FIFO_CTRL3
+BDR_XL3 = 0x08
+BDR_XL2 = 0x04
+BDR_XL1 = 0x02
+BDR_XL0 = 0x01
+
+#FIFO_CTRL4
+
+FIFO_MODE2 = 0x04
+FIFO_MODE1 = 0x02
+FIFO_MODE0 = 0x01
+
+
+#MD1_CFG 
+
+INT1_SHUB = 0x01
+
+#FUNC_CFG_ACCESS
+
+SHUB_REG_ACCESS = 0x40
+
+
+#SLAV0_CONFIG
+BATCH_EXT_SENS_0_EN = 0x08
+Slave0_numop2 = 0x04
+Slave0_numop1 = 0x02
+Slave0_numop0 = 0x01
+
+#MASTER_CONFIG
+WRITE_ONCE = 0x40
+SHUB_PU_EN = 0x08
+MASTER_ON  = 0x04
+AUX_SENS_ON1 = 0x02
+AUX_SENS_ON0 = 0x01
 
 def one_shot_write(register, value):
-	bus.write_byte_data(0x6a,FUNC,0x40)
-	bus.write_byte_data(0x6a,SLAV,slave_addr_write)
-	bus.write_byte_data(0x6a,SLAVS,register)
-	bus.write_byte_data(0x6a,SLAV_CONF,0x00)
-	bus.write_byte_data(0x6a,DATAW,value)
-	bus.write_byte_data(0x6a,MAST,0x4C)
-	while (not bus.read_byte_data(0x6a,STAT)>>7):
+	bus.write_byte_data(IMU_addr,FUNC_CFG_ACCESS,SHUB_REG_ACCESS)
+	bus.write_byte_data(IMU_addr,SLAV,slave_addr_write)
+	bus.write_byte_data(IMU_addr,SLAVS,register)
+	bus.write_byte_data(IMU_addr,SLAV_CONF,0x00)
+	bus.write_byte_data(IMU_addr,DATAW,value)
+	bus.write_byte_data(IMU_addr,MASTER_CONFIG,WRITE_ONCE|SHUB_PU_EN|MASTER_ON)
+	while (not bus.read_byte_data(IMU_addr,STAT)>>7):
 		continue
-	bus.write_byte_data(0x6a,MAST,0x08)
+	bus.write_byte_data(IMU_addr,MASTER_CONFIG,SHUB_PU_EN)
 	time.sleep(0.0003)
-	bus.write_byte_data(0x6a,FUNC,0x00)
+	bus.write_byte_data(IMU_addr,FUNC_CFG_ACCESS,0x00)
 
 def one_shot_read():
-	bus.write_byte_data(0x6a,FUNC,0x40)
-	bus.write_byte_data(0x6a,SLAV,slave_addr_read)
-	bus.write_byte_data(0x6a,SLAVS,ACCX)
-	bus.write_byte_data(0x6a,SLAV_CONF,0x0E)
-	bus.write_byte_data(0x6a,MAST,0x4C)
-	bus.write_byte_data(0x6a,FUNC,0x00)
+	bus.write_byte_data(IMU_addr,FUNC_CFG_ACCESS,SHUB_REG_ACCESS)
+	bus.write_byte_data(IMU_addr,SLAV,slave_addr_read)
+	bus.write_byte_data(IMU_addr,SLAVS,ACCX)
+	# 2nd IMU data is store in FIFO at 104 HZ
+	bus.write_byte_data(IMU_addr,SLAV_CONF,BATCH_EXT_SENS_0_EN|Slave0_numop2|Slave0_numop1)
+	bus.write_byte_data(IMU_addr,MASTER_CONFIG,WRITE_ONCE|SHUB_PU_EN|MASTER_ON)
+	bus.write_byte_data(IMU_addr,FUNC_CFG_ACCESS,0x00)
 	
 	
+# 1st IMU Accelerometor data on INT1
+bus.write_byte_data(IMU_addr,INT1,INT1_DRDY_XL)
+# 1st IMU set Accelerometor data rate 104 Hz
+bus.write_byte_data(IMU_addr,CTRL1,ODR_XL2)
 
-bus.write_byte_data(0x6a,INT1,0x01)
-bus.write_byte_data(0x6a,CTRL1,0x40)
+# one shot write to INT1 on 2nd IMU 
+# 2nd IMU Accelerometor data on INT1
+one_shot_write(INT1,INT1_DRDY_XL)
 
-# one shot write to INT1 on slave IMU 
-one_shot_write(INT1,0x01)
+# One shot write to CTRL1 on 2nd IMU 
+# 2nd IMU set Accelerometor data rate 104 Hz
+one_shot_write(CTRL1,ODR_XL2)
 
-# One shot write to CTRL
-one_shot_write(CTRL1,0x60)
+# accelerometer data write into FIFO at 104 Hz
+bus.write_byte_data(IMU_addr,FIFO_CTRL3,BDR_XL2)
+# set FIFO in continuous mode.
+bus.write_byte_data(IMU_addr,FIFO_CTRL4,FIFO_MODE2|FIFO_MODE1)
 
-bus.write_byte_data(0x6a,FIFO_CTRL3,0x04)
-
-bus.write_byte_data(0x6a,FIFO_CTRL4,0x06)
-bus.write_byte_data(0x6a,0x5E,0x01)
+# 1st IMU sensor hub data-ready signal on INT1
+bus.write_byte_data(IMU_addr,MD1_CFG,INT1_SHUB)
 
 def read_data(register):
-	accx = bus.read_word_data(0x6a,register)
+	accx = bus.read_word_data(IMU_addr,register)
 	return accx
 
 
-
+# start the I2C between IMU 
 one_shot_read()
 
 data1_count = 0
@@ -103,13 +164,16 @@ def animate(i):
 	#f= open("output.txt","w+")
 	global data1_count
 	global data2_count
-
-	while ( count <= 100) :
-		level = bus.read_byte_data(0x6a,0x3A)
-		
-		IMU2 = bus.read_i2c_block_data(0x6a,0x78,7)
+	level = bus.read_byte_data(IMU_addr,FIFO_STATUS1)
+	print(level)
+	while ( level != 0 or count <= 104) :
+		#read the FIFO level
+		level = bus.read_byte_data(IMU_addr,FIFO_STATUS1)
+		print(level)
+		# read the tag and 6 continous byte FIFO data
+		IMU2 = bus.read_i2c_block_data(IMU_addr,FIFO_DATA_OUT_TAG,7)
 		print(IMU2)
-		#f.write(str(IMU2) + "\n")
+		
 		if IMU2[6] != 255 and IMU2[6] != 253:
 			if len(hex(IMU2[5])) == 3:
 				z_data = int(hex(IMU2[6])[2:]+'0'+ hex(IMU2[5])[2:],16)
